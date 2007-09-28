7b7fc357254feaa87442bf400d37267dbe58ab2b
sdrac8 <sdrac8@gmail.com>
2007-09-28 06:26:00 +0000

    start of fullscreen capability
    adds full screen capability but still at a fixed 1000x1000 pixels
    adds the option -F=true or --fullscreen=true to set full screen

--- netrek.py
+++ netrek.py
@@ -137,6 +137,7 @@ cp `grep IMAGERY try.py|grep -v grep|cut -f2 -d:|sort|uniq` /tmp/netrek-client-p
 import sys, time, socket, select, struct, pygame, math
 from Cache import *
 from Constants import *
+from pygame.locals import * #need this for 'FULLSCREEN' 
 
 print "Netrek Client Pygame"
 print "Copyright (C) 2007 James Cameron <quozl@us.netrek.org>"
@@ -148,6 +149,8 @@ print ""
 
 from optparse import OptionParser
 parser= OptionParser()
+parser.add_option("-F", "--fullscreen", dest="fullscreen",
+                  help="forces client into fullscreen mode")
 parser.add_option("-s", "--server", "--host", dest="server",
                   help="netrek server to connect to")
 parser.add_option("-p", "--port", type="int", dest="port", default="2592",
@@ -2880,11 +2883,15 @@ class PhaseFlightTactical(PhaseFlight):
 """
 
 pygame.init()
-
 size = width, height = 1000, 1000
 screen = pygame.display.set_mode(size)
+# FIXME: #1187736408 support a full screen mode that's variable depending on the
+# environment
+if opt.fullscreen :
+    pygame.display.set_mode(size, FULLSCREEN)
+    
+
 # FIXME: #1187736407 support screen resolutions below 1000x1000
-# FIXME: #1187736408 support a full screen mode
 
 t_planets = pygame.sprite.OrderedUpdates(())
 t_players = pygame.sprite.OrderedUpdates(())
