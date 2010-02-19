d98a79290fe3dddbc97ba153304640ff45e5bbff
quozl <quozl@us.netrek.org>
2010-02-19 02:01:25 +0000

    add team colour function, fix reserved words

--- netrek/util.py
+++ netrek/util.py
@@ -1,31 +1,40 @@
 from constants import *
 
-def strnul(input):
+def strnul(x):
     """ convert a NUL terminated string to a normal string
     """
-    return input.split('\000')[0]
+    return x.split('\000')[0]
 
-def team_decode(input):
+def team_decode(mask):
     """ convert a team mask to a list
     """
     x = []
-    if input & FED: x.append(teams[FED])
-    if input & ROM: x.append(teams[ROM])
-    if input & KLI: x.append(teams[KLI])
-    if input & ORI: x.append(teams[ORI])
+    if mask & FED: x.append(teams[FED])
+    if mask & ROM: x.append(teams[ROM])
+    if mask & KLI: x.append(teams[KLI])
+    if mask & ORI: x.append(teams[ORI])
     return x
 
-def race_decode(input):
+def race_decode(n):
     """ convert a race number to letter
     """
-    if input == 0: return 'F'
-    elif input == 1: return 'R'
-    elif input == 2: return 'K'
-    elif input == 3: return 'O'
+    if n == 0: return 'F'
+    elif n == 1: return 'R'
+    elif n == 2: return 'K'
+    elif n == 3: return 'O'
     return 'I'
 
-def slot_decode(input):
+def slot_decode(n):
     try:
-        return '0123456789abcdefghijklmnopqrstuvwxyz'[input]
+        return '0123456789abcdefghijklmnopqrstuvwxyz'[n]
     except IndexError:
-        print "slot_decode: input value from server %d out of range" % input
+        print "slot_decode: input value from server %d out of range" % n
+
+def team_colour(team):
+    """ convert a team mask with a single bit set to a colour
+    """
+    if team == FED: return (128, 128, 0)
+    if team == ROM: return (128, 0, 0)
+    if team == KLI: return (0, 128, 0)
+    if team == ORI: return (0, 128, 128)
+    return (128, 128, 128)
